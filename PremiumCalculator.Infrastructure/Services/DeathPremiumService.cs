84d3b0dddf26ab861c955a3d9faa41b1de376e99
rakesh.bairi <bairi.rakesh@gmail.com>
2023-05-26 02:40:04 +0530

    Init Commit

---
+++ PremiumCalculator.Infrastructure/Services/DeathPremiumService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PremiumCalculator.Infrastructure.Data;
+using PremiumCalculator.Infrastructure.Models;
+using PremiumCalculator.Infrastructure.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PremiumCalculator.Infrastructure.Services
+{
+    public class DeathPremiumService : IDeathPremiumCalculate
+    {
+        //https://stackoverflow.com/questions/39174989/how-to-register-multiple-implementations-of-the-same-interface-in-asp-net-core
+        public float PremiumCalculate(InsuredPerson insuredPerson)
+        {
+            var ratingFactor =  (from o in DataService.Occupations()
+                                join ocrating in DataService.OccupationRating() on o.RatingID equals ocrating.RatingID
+                                where o.OccupationID == insuredPerson.OccupationID
+                                select ocrating.Factor).FirstOrDefault();
+
+            return (insuredPerson.SumInsured * insuredPerson.Age* ratingFactor) / 1000 * 12;
+        }
+    }
+}
